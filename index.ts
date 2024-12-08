9d20607c1502455e784c9bb8599bd9961c202d60
Oleksii Yatsenko <yatsenkolesh@gmail.com>
2024-12-08 09:48:11 +0000

    add stat

--- index.ts
+++ index.ts
@@ -3,12 +3,47 @@ import ProcessMessages from "./services/MessageProcessorService";
 import {ConversationStorage} from "./services/ConversationStorage";
 
 
-
+/**
+ * todo refactor to stat + move lastIteration to internal function
+ */
+let iterations: number = 0
+let lastIteration:string = ""
 const config:Config = GetConfig()
 
 let store: ConversationStorage = new ConversationStorage()
 const messaging = () => {
+    iterations++
+    if (iterations > 1000) {
+        iterations = 1
+    }
+
+    lastIteration = new Date().toISOString()
     ProcessMessages(config, store)
 };
 
 setInterval(messaging, 1000)
+
+//create server
+const { createServer } = require('node:http');
+
+
+const hostname = '127.0.0.1';
+const port = 5222;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    let responseJson : {
+        iterations: number,
+        lastIteration: string
+    } = {
+        iterations: iterations,
+        lastIteration: lastIteration
+    };
+
+    res.end(JSON.stringify(responseJson));
+});
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
